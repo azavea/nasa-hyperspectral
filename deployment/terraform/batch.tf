abc89055223cfa98c23998aded33f96cfc19d003
Rocky Breslow <rbreslow@azavea.com>
2020-12-07 12:05:13 -0500

    Set up AWS Batch infrastructure
    Add Terraform configuration for an AWS Batch compute environment. This
    includes supporting resources such as a job queue, an example job
    definition, an EC2 launch template, security group rules, and IAM roles.

---
+++ deployment/terraform/batch.tf
@@ -0,0 +1,94 @@
+#
+# Security Group resources
+#
+resource "aws_security_group" "batch" {
+  name   = "sgBatchContainerInstance"
+  vpc_id = module.vpc.id
+
+  tags = {
+    Name        = "sgBatchContainerInstance"
+    Project     = var.project
+    Environment = var.environment
+  }
+}
+
+# Pull the image ID for the latest Amazon ECS optimized AMI
+# https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html
+data "aws_ssm_parameter" batch_ami_id {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+}
+
+#
+# Batch resources
+#
+resource "aws_launch_template" "default" {
+  name_prefix = "ltBatchContainerInstance-"
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = var.batch_root_block_device_size
+      volume_type = var.batch_root_block_device_type
+    }
+  }
+
+  user_data = base64encode(file("cloud-config/batch-container-instance"))
+}
+
+resource "aws_batch_compute_environment" "default" {
+  compute_environment_name_prefix = "batch${replace(var.project, " ", "")}"
+  type                            = "MANAGED"
+  state                           = "ENABLED"
+  service_role                    = aws_iam_role.batch_service_role.arn
+
+  compute_resources {
+    type                = "SPOT"
+    allocation_strategy = var.batch_spot_fleet_allocation_strategy
+    bid_percentage      = var.batch_spot_fleet_bid_percentage
+    ec2_key_pair        = var.aws_key_name
+    image_id            = data.aws_ssm_parameter.batch_ami_id.value
+
+    min_vcpus = var.batch_min_vcpus
+    max_vcpus = var.batch_max_vcpus
+
+    launch_template {
+      launch_template_id = aws_launch_template.default.id
+      version            = aws_launch_template.default.latest_version
+    }
+
+    spot_iam_fleet_role = aws_iam_role.spot_fleet_service_role.arn
+    instance_role       = aws_iam_instance_profile.ecs_instance_role.arn
+
+    instance_type = var.batch_instance_types
+
+    security_group_ids = [aws_security_group.batch.id]
+    subnets            = module.vpc.private_subnet_ids
+
+    tags = {
+      Name        = "BatchWorker"
+      Project     = var.project
+      Environment = var.environment
+    }
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.batch_service_role_policy]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_batch_job_queue" "default" {
+  name                 = "queue${replace(var.project, " ", "")}"
+  priority             = 1
+  state                = "ENABLED"
+  compute_environments = [aws_batch_compute_environment.default.arn]
+}
+
+resource "aws_batch_job_definition" "download_test" {
+  name = "job${replace(var.project, " ", "")}DownloadTest"
+  type = "container"
+
+  container_properties = templatefile("${path.module}/job-definitions/download-test.json.tmpl", {})
+}
--- deployment/terraform/firewall.tf
+++ deployment/terraform/firewall.tf
@@ -135,3 +135,36 @@ resource "aws_security_group_rule" "franklin_rds_egress" {
   security_group_id        = aws_security_group.franklin.id
   source_security_group_id = module.database.database_security_group_id
 }
+
+#
+# Batch container instance security group resources
+#
+resource "aws_security_group_rule" "batch_http_egress" {
+  type        = "egress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.batch.id
+}
+
+resource "aws_security_group_rule" "batch_https_egress" {
+  type        = "egress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.batch.id
+}
+
+resource "aws_security_group_rule" "batch_bastion_ingress" {
+  type      = "ingress"
+  from_port = 22
+  to_port   = 22
+  protocol  = "tcp"
+
+  security_group_id        = aws_security_group.batch.id
+  source_security_group_id = module.vpc.bastion_security_group_id
+}
