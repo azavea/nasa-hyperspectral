ce5641f8ed3553138c188ffbbcc69fa44a46bd2b
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-01-07 18:13:25 -0500

    Add comments into the code

--- src/pipeline/activator/cog-clip/src/main/scala/com/azavea/nasa/hsi/commands/CogClip.scala
+++ src/pipeline/activator/cog-clip/src/main/scala/com/azavea/nasa/hsi/commands/CogClip.scala
@@ -13,15 +13,16 @@ import cats.syntax.flatMap._
 import cats.effect.syntax.paralleln._
 import com.azavea.nasa.hsi.s3.S3Configuration
 import com.azavea.stac4s.StacItem
-import geotrellis.proj4.{CRS, LatLng}
+import geotrellis.proj4.LatLng
 import geotrellis.raster.RasterSource
 import geotrellis.raster.io.geotiff._
 import geotrellis.vector.{io => _, _}
 import geotrellis.store.hadoop._
 import io.chrisdavenport.log4cats.Logger
 import io.circe.Json
-import sttp.client3.SttpBackend
+import sttp.client3.{HttpError, SttpBackend}
 import sttp.client3.asynchttpclient.cats.AsyncHttpClientCatsBackend
+import sttp.model.StatusCode
 
 import java.io.IOException
 import scala.util.Try
@@ -32,32 +33,39 @@ object CogClip {
     for {
       _ <- Logger[F].info(s"Retrieving item ${config.sourceItemId} from the catalog ${config.sourceCollectionId}")
       client = SttpStacClient(backend, config.stacApiURI.toSttpUri)
+      // request an item
       item <- client.item(config.sourceCollectionId, config.sourceItemId)
       result <- item match {
+        // if such item is present in the catalog
         case Some(item) if item.assets.isDefinedAt(config.sourceAssetId.value) =>
           val asset = item.assets(config.sourceAssetId.value)
           val rs    = RasterSource(asset.hrefGDAL(config.withGDAL))
 
+          // traverse through all input features and window read each geometry
           config.features
             .getAllFeatures[Feature[Geometry, Json]]
             .toList
             .parTraverseN(config.threads.value) { feature =>
+              // check if features is already present in the target collection
               feature.data
                 .randomIdF[F]
                 .flatMap { featureId =>
                   client
                     .item(config.targetCollectionId, config.resultId(featureId))
-                    .recoverWith { case _ =>
-                      Logger[F]
-                        .trace(s"Item ${config.resultId(featureId)} doesn't exist in the collection ${config.targetCollectionId}")
-                        .as(None)
+                    .recoverWith {
+                      case e: HttpError[_] if e.statusCode == StatusCode.NotFound =>
+                        Logger[F]
+                          .trace(s"Item ${config.resultId(featureId)} doesn't exist in the collection ${config.targetCollectionId}")
+                          .as(None)
                     }
                 }
                 .flatMap {
+                  // if the target item is already present in the target collection, than do nothing
                   case Some(oldItem) =>
                     Logger[F]
                       .info(s"Item ${oldItem.id} is already present in the collection ${config.targetCollectionId}")
                       .as(oldItem)
+                  // if it is new, read and write tiff
                   case _ =>
                     Try(feature.geom.extent.reproject(LatLng, rs.crs))
                       .liftTo[F]
@@ -84,8 +92,11 @@ object CogClip {
     client: StacClient[F]
   ): F[StacItem] =
     feature.data.randomIdF[F].flatMap { featureId =>
+      // write tiff
       Logger[F].trace(s"Writing tiff to ${config.cogAssetHrefLocal(featureId)}") >>
       S3Configuration.defaultSync[F].map(geotiff.write(config.cogAssetHrefLocal(featureId), _)) >>
+      // create [[StacItem]] and insert it into the target collection
+      // it creates the target collection if it's missing
       DefaultCollection
         .item(config, featureId, item.id, feature.geom)
         .pure
--- src/pipeline/activator/cog-clip/src/main/scala/com/azavea/nasa/hsi/s3/S3Configuration.scala
+++ src/pipeline/activator/cog-clip/src/main/scala/com/azavea/nasa/hsi/s3/S3Configuration.scala
@@ -4,11 +4,11 @@ import cats.effect.Sync
 import org.apache.hadoop.conf.Configuration
 
 object S3Configuration {
-  lazy val DEFAULT = {
+  lazy val DEFAULT: Configuration = {
     val conf = new Configuration()
     conf.set("fs.s3.impl", classOf[org.apache.hadoop.fs.s3a.S3AFileSystem].getName)
     conf.set("fs.s3a.aws.credentials.provider", classOf[com.amazonaws.auth.DefaultAWSCredentialsProviderChain].getName)
-    conf.set("fs.s3a.endpoint", "s3.eu-west-2.amazonaws.com")
+    conf.set("fs.s3a.endpoint", "s3.es-east-1.amazonaws.com")
     conf
   }
 
