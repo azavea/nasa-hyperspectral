3161d45a7612284b45684b9c8ed34f24376acbd5
Rocky Breslow <rbreslow@azavea.com>
2021-01-18 15:15:16 -0500

    Codify data bucket

--- deployment/terraform/storage.tf
+++ deployment/terraform/storage.tf
@@ -1,3 +1,19 @@
+#
+# S3 resources
+#
+resource "aws_s3_bucket" "data" {
+  bucket = "${lower(replace(var.project, " ", ""))}-${lower(var.environment)}-data-${var.aws_region}"
+
+  tags = {
+    Name        = "${lower(replace(var.project, " ", ""))}-${lower(var.environment)}-data-${var.aws_region}"
+    Project     = var.project
+    Environment = var.environment
+  }
+}
+
+#
+# ECR resources
+#
 module "activator_aviris_l2" {
   source = "github.com/azavea/terraform-aws-ecr-repository?ref=1.0.0"
 
