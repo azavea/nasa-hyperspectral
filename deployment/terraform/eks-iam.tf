b1d97df622c46f004cbbc79006bd11a18416a4cd
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-07-29 13:26:10 -0400

    EKS Workers Autoscaling

--- deployment/terraform/eks-iam.tf
+++ deployment/terraform/eks-iam.tf
@@ -40,3 +40,62 @@ resource "aws_iam_policy" "worker_cloudwatch_policy" {
   name   = "cloudWatchEksWorkerPolicy"
   policy = data.aws_iam_policy_document.worker_cloudwatch_policy_document.json
 }
+
+# https://github.com/terraform-aws-modules/terraform-aws-eks/blob/v17.1.0/docs/autoscaling.md
+resource "aws_iam_role_policy_attachment" "workers_autoscaling" {
+  policy_arn = aws_iam_policy.worker_autoscaling.arn
+  role       = module.eks.worker_iam_role_name
+}
+
+resource "aws_iam_policy" "worker_autoscaling" {
+  name_prefix = "eks-worker-autoscaling-${module.eks.cluster_id}"
+  description = "EKS worker node autoscaling policy for cluster ${module.eks.cluster_id}"
+  policy      = data.aws_iam_policy_document.worker_autoscaling.json
+  path        = "/"
+  tags = {
+    Name        = "EKSAutoScaledWorker"
+    Project     = var.project
+  }
+}
+
+data "aws_iam_policy_document" "worker_autoscaling" {
+  statement {
+    sid    = "eksWorkerAutoscalingAll"
+    effect = "Allow"
+
+    actions = [
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeAutoScalingInstances",
+      "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:DescribeTags",
+      "ec2:DescribeLaunchTemplateVersions",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "eksWorkerAutoscalingOwn"
+    effect = "Allow"
+
+    actions = [
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "autoscaling:UpdateAutoScalingGroup",
+    ]
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "autoscaling:ResourceTag/kubernetes.io/cluster/${module.eks.cluster_id}"
+      values   = ["owned"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/enabled"
+      values   = ["true"]
+    }
+  }
+}
