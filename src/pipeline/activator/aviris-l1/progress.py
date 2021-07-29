5390d825b37ce12f19f8278f651835ee95c39a37
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-07-29 19:30:13 -0400

    Rollback progressbar and update workflow yaml

--- src/pipeline/activator/aviris-l1/progress.py
+++ src/pipeline/activator/aviris-l1/progress.py
@@ -5,12 +5,7 @@ import sys
 import threading
 from time import time
 from tqdm import tqdm
-import logging
 
-LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO').upper()
-logging.basicConfig(
-    format='[%(relativeCreated)d|%(levelname)s|%(name)s|%(lineno)d] %(message)s', level=LOG_LEVEL)
-logger = logging.getLogger(__name__)
 
 class ProgressPercentage(object):
     """ Callable object for use with s3.upload_object callback """
@@ -27,13 +22,14 @@ class ProgressPercentage(object):
         with self._lock:
             self._seen_so_far += bytes_amount
             percentage = (self._seen_so_far / self._size) * 100
-            logger.info(
+            sys.stdout.write(
                 "\r{}  {} / {} ({:.2f}%)".format(
                     self._filename, self._seen_so_far, self._size, percentage
                 )
             )
+            sys.stdout.flush()
             if percentage >= 100:
-                logger.info("")
+                print("")
 
 
 @contextmanager
@@ -42,14 +38,14 @@ def timing(description: str) -> None:
     start = time()
     yield
     elapsed = time() - start
-    logger.info("{}: {:.4f}s".format(description, elapsed))
+    print("{}: {:.4f}s".format(description, elapsed))
 
 
 def warp_callback(progress, *args):
     """ Report progress for GDALWarp callback argument """
     progress_pct = floor(progress * 100)
     if progress_pct % 10 == 0 > progress_pct > 0:
-        logger.info(f'GDAL Warp: {progress_pct}%')
+        print("GDAL Warp: {}%".format(progress_pct))
 
 
 class DownloadProgressBar(tqdm):
