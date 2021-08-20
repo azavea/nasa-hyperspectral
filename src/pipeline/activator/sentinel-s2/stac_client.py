1d70767a7b87cc9d9abfce4dd5b7b9c8931483c3
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-08-19 20:46:47 -0400

    Add sentinel

---
+++ src/pipeline/activator/sentinel-s2/stac_client.py
@@ -0,0 +1,65 @@
+import json
+
+import pystac
+import requests
+
+
+class STACClient:
+    def __init__(self, stac_api_url):
+        """ Create a new STACClient for stac_api_url
+
+        stac_api_url should include scheme, hostname, and port if applicable
+        Example: http://localhost:9090
+
+        """
+        self.stac_api_url = str(stac_api_url)
+
+    def get_collection(self, collection_id):
+        """ GET item pystac.Collection from STAC API collection_id """
+        url = "{}/collections/{}".format(
+            self.stac_api_url, collection_id
+        )
+        response = requests.get(url)
+        response.raise_for_status()
+        return pystac.Collection.from_dict(response.json())
+
+    def get_collection_item(self, collection_id, item_id):
+        """ GET item pystac.Item from STAC API collection_id """
+        url = "{}/collections/{}/items/{}".format(
+            self.stac_api_url, collection_id, item_id
+        )
+        response = requests.get(url)
+        response.raise_for_status()
+        return pystac.Item.from_dict(response.json())
+
+    def has_collection(self, collection_id):
+        """ Return True if STAC API has collection_id Collection else False """
+        response = requests.get(
+            "{}/collections/{}".format(self.stac_api_url, collection_id)
+        )
+        return True if response.status_code < 300 else False
+
+    def post_collection(self, collection):
+        """ POST collection pystac.Collection to STAC API """
+        collection_dict = collection.to_dict()
+        collection_dict['type'] = 'Collection'
+        collection_dict['stac_version'] = '1.0.0'
+        response = requests.post(
+            "{}/collections".format(self.stac_api_url),
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(collection_dict),
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def post_collection_item(self, collection_id, item):
+        """ POST item pystac.Item to collection_id """
+        item_dict = item.to_dict()
+        item_dict['stac_version'] = '1.0.0'
+        response = requests.post(
+            "{}/collections/{}/items".format(self.stac_api_url, collection_id),
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(item_dict),
+        )
+        response.raise_for_status()
+        return response.json()
